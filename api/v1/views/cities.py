b8fe4e6919360f89ad13e93e6efe47afaedc89e0
VieetBubbles <537@holbertonschool.com>
2019-09-11 23:24:18 +0000

    Uncommented import cities inorder to use city routes

---
+++ api/v1/views/cities.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+
+from flask import Flask, make_response, jsonify, request, abort
+from models import storage
+from models.state import State
+from models.city import City
+from api.v1.views import app_views
+import sys
+
+
+@app_views.route('/states/<state_id>/cities', methods=['GET'],
+                 strict_slashes=False)
+def city_no_id_get(state_id):
+    state = storage.get("State", state_id)
+
+    if state is None:
+        abort(404)
+
+    all_cities = []
+    for city in state.cities:
+        all_cities.append(city.to_dict())
+    return jsonify(all_cities)
+
+
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
+def city_no_id_post(state_id):
+    state = storage.get("State", state_id)
+    json_string_dict = request.get_json()
+
+    if state is None:
+        abort(404)
+
+    if json_string_dict is None:
+        return make_response(jsonify('Not a JSON'), 400)
+    if 'name' not in json_string_dict:
+        return make_response(jsonify('Missing name'), 400)
+
+    json_string_dict['state_id'] = state_id
+    city_object = City(**json_string_dict)
+    city_object.save()
+    return make_response(jsonify(city_object.to_dict()), 201)
+
+
+@app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
+def city_with_id(city_id):
+    city = storage.get("City", city_id)
+    if city is None:
+        abort(404)
+    return jsonify(city.to_dict())
+
+
+@app_views.route('/cities/<city_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def city_id_delete(city_id):
+    city = storage.get("City", city_id)
+
+    if city is None:
+        abort(404)
+    city.delete()
+    del city
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
+def city_id_put(city_id):
+    city = storage.get("City", city_id)
+    json_string_dict = request.get_json()
+
+    if city is None:
+        abort(404)
+    if json_string_dict is None:
+        return make_response(jsonify('Not a JSON'), 400)
+
+    for key, value in json_string_dict.items():
+        if key not in ['id', 'state_id', 'created_at', 'updated_at']:
+            setattr(city, key, value)
+    city.save()
+    return jsonify(city.to_dict())
