2e6ef351c2266175e8d286fd3509fa8e93d01bb5
VieetBubbles <537@holbertonschool.com>
2019-09-12 02:57:48 +0000

    Fixed output of dictionary typo

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,10 +15,17 @@ def uuiui():
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats_all():
     """method that retrieves the number of each object"""
-    classes = ["Amenity", "City", "Place", "Review", "State", "User"]
     d = {}
+    classes = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+    }
 
-    for i in classes:
-        c = storage.count(i)
-        d[i] = c
+    for key, value in classes.items():
+        c = storage.count(key)
+        d[value] = c
     return jsonify(d)
