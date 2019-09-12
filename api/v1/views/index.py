16491d8de5f7d448bf26f70089d58a9b53714721
VieetBubbles <537@holbertonschool.com>
2019-09-12 02:09:06 +0000

    Added the GET method to the route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,17 +6,13 @@ from models import storage
 import json
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def uuiui():
     """displays status ok"""
-    r = {"status": "OK"}
-    r = json.dumps(r)
-    loaded_r = json.loads(r)
-
-    return (loaded_r)
+    return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', strict_slashes=False)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats_all():
     """method that retrieves the number of each object"""
     classes = ["Amenity", "City", "Place", "Review", "State", "User"]
