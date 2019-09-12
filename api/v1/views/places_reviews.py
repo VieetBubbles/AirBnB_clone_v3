d2876040bfacc85f29299f3c8f68182a78fe0a4a
VieetBubbles <537@holbertonschool.com>
2019-09-12 22:36:58 +0000

    Importted the user class

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -5,6 +5,7 @@ from models import storage
 from models.state import State
 from models.city import City
 from models.place import Place
+from models.user import User
 from models.review import Review
 from api.v1.views import app_views
 import sys
